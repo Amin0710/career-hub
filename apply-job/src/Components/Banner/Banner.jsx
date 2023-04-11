2fc15e7de83d9316c29ee591074b0adb0d962619
Nurul Amin <nurul.amin.naim@gmail.com>
2023-04-12 01:02:14 +1000

    Banner done

--- apply-job/src/Components/Banner/Banner.jsx
+++ apply-job/src/Components/Banner/Banner.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import "./Banner.css";
+import bannerMan from "../../assets/All-Images/P3OLGJ1.png";
 
 const Banner = () => {
 	return (
-		<div className="header">
-			<div>aa</div>
-			<div>
-				<img src="../../assets/All-Images/P3OLGJ1.png" alt="" />
+		<div className="banner-back">
+			<div className="banner">
+				<div className="banner-words">
+					<div className="banner-words-content">
+						<p id="bigHead">
+							One Step Closer To Your
+							<span id="bigHead-highlight"> Dream Job</span>
+						</p>
+						<p id="smallHead">
+							Explore thousands of job opportunities with all the information
+							you need. Its your future. Come find it. Manage all your job
+							application from start to finish.
+						</p>
+						<button>Get Started</button>
+					</div>
+				</div>
+				<div>
+					<img src={bannerMan} alt="" />
+				</div>
 			</div>
 		</div>
 	);
