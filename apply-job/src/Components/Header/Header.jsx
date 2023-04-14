b98536f09bf17bbe3406f42e53362ab5f6498d41
Nurul Amin <nurul.amin.naim@gmail.com>
2023-04-14 16:53:11 +1000

    nav responsive

--- apply-job/src/Components/Header/Header.jsx
+++ apply-job/src/Components/Header/Header.jsx
@@ -1,20 +1,31 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import menu from "../../assets/All-Images/menu.png";
 import ActiveLink from "../Activelink/Activelink";
 import "./Header.css";
 
 const Header = () => {
+	const showLinks = () => {
+		const x = document.getElementById("links");
+		if (x.style.display === "none") {
+			x.style.display = "flex";
+		} else {
+			x.style.display = "none";
+		}
+	};
 	return (
 		<header className="header">
-			<nav>
+			<nav id="nav">
 				<div className="logo">ApplyJob</div>
-				<div>
+				<div onClick={showLinks} className="semi-none">
+					<img src={menu} alt="" />
+				</div>
+				<div id="links">
 					<ActiveLink to="/">Home</ActiveLink>
 					<ActiveLink to="/Statistics">Statistics</ActiveLink>
 					<ActiveLink to="/AppliedJobs">Applied Jobs</ActiveLink>
 					<ActiveLink to="/Blog">Blog</ActiveLink>
 				</div>
-				<button>Star Applying</button>
+				<button className="none">Star Applying</button>
 			</nav>
 		</header>
 	);
