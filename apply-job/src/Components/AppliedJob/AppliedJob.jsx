2044e1a46e03989697625e11419aaf6918b624ec
Nurul Amin <nurul.amin.naim@gmail.com>
2023-04-15 19:23:37 +1000

    filter done

--- apply-job/src/Components/AppliedJob/AppliedJob.jsx
+++ apply-job/src/Components/AppliedJob/AppliedJob.jsx
@@ -4,40 +4,44 @@ import locationImg from "../../assets/Icons/Frame-4.png";
 import moneyImg from "../../assets/Icons/Frame.png";
 import { Link } from "react-router-dom";
 
-const AppliedJob = ({ appliedJobs, index }) => {
-	const id = appliedJobs[index].JobId;
+const AppliedJob = ({ filteredAppliedJobs, index }) => {
+	const id = filteredAppliedJobs[index].JobId;
 
 	return (
 		<div className="appliedJob">
 			<div className="appliedJobLogo">
 				<img
 					style={{ maxWidth: "238px" }}
-					src={appliedJobs[index].CompanyLogo}
+					src={filteredAppliedJobs[index].CompanyLogo}
 					alt=""
 				/>
 			</div>
 			<div className="flexBetween">
 				<div>
-					<p className="FeaturedName">{appliedJobs[index].JobTitle}</p>
-					<p className="FeaturedDetails">{appliedJobs[index].CompanyName}</p>
+					<p className="FeaturedName">{filteredAppliedJobs[index].JobTitle}</p>
+					<p className="FeaturedDetails">
+						{filteredAppliedJobs[index].CompanyName}
+					</p>
 					<div className="flex">
 						<p className="FeaturedDetailsBox">
-							{appliedJobs[index].JobLocation}
+							{filteredAppliedJobs[index].JobLocation}
 						</p>
-						<p className="FeaturedDetailsBox">{appliedJobs[index].JobType}</p>
+						<p className="FeaturedDetailsBox">
+							{filteredAppliedJobs[index].JobType}
+						</p>
 					</div>
 					<div className="flex">
 						<div className="flex">
 							<img className="detailsIconImg" src={locationImg} alt="" />
 							<p className="FeaturedDetails">
-								{appliedJobs[index].CompanyLocation.city},{" "}
-								{appliedJobs[index].CompanyLocation.country}
+								{filteredAppliedJobs[index].CompanyLocation.city},{" "}
+								{filteredAppliedJobs[index].CompanyLocation.country}
 							</p>
 						</div>
 						<div className="flex">
 							<img className="detailsIconImg" src={moneyImg} alt="" />
 							<p className="FeaturedDetails">
-								Salary : {appliedJobs[index].Salary}
+								Salary : {filteredAppliedJobs[index].Salary}
 							</p>
 						</div>
 					</div>
--- apply-job/src/Components/AppliedJobs/AppliedJobs.jsx
+++ apply-job/src/Components/AppliedJobs/AppliedJobs.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useLoaderData } from "react-router-dom";
+import { showHide } from "../../Utilities/showHide";
 import AppliedJob from "../AppliedJob/AppliedJob";
 import SubBanner from "../SubBanner/SubBanner";
 import "./AppliedJobs.css";
@@ -7,17 +8,40 @@ import "./AppliedJobs.css";
 const AppliedJobs = () => {
 	const appliedJobs = useLoaderData();
 	const Head = "Applied Jobs";
+
+	const [filterOption, setFilterOption] = useState("none");
+	const [filteredAppliedJobs, setFilteredAppliedJobs] = useState(appliedJobs);
+
+	const handleClick1 = () => {
+		setFilterOption("Remote");
+	};
+	const handleClick2 = () => {
+		setFilterOption("Onsite");
+	};
+
+	useEffect(() => {
+		setFilteredAppliedJobs(
+			appliedJobs.filter((j) => j.JobLocation !== filterOption)
+		);
+	}, [appliedJobs, filterOption]);
+
 	return (
 		<div>
 			<SubBanner Head={Head}></SubBanner>
 			<div className="appliedJobContainer">
-				<div id="FilterBy">
+				<div
+					className="FilterBy"
+					onClick={() => showHide("FilterOption", "flex")}>
 					<button>Filter By ⬇️</button>
 				</div>
-				{appliedJobs.map((j, i) => (
+				<div className="FilterBy" id="FilterOption">
+					<button onClick={handleClick1}>Onsite</button>
+					<button onClick={handleClick2}>Remote</button>
+				</div>
+				{filteredAppliedJobs.map((j, i) => (
 					<AppliedJob
 						key={j.JobId}
-						appliedJobs={appliedJobs}
+						filteredAppliedJobs={filteredAppliedJobs}
 						index={i}></AppliedJob>
 				))}
 			</div>
